d37fa9d906add6f51e503275036a1874501fdc45
Jonathan Huang <jonathannhuang@gmail.com>
2023-11-25 01:04:06 -0500

    finished the toggleText button that allows to add more text off button press

--- layout/scripts/script.js
+++ layout/scripts/script.js
@@ -1,10 +1,11 @@
 function toggleText(id) {
     var text = document.getElementById(id);
-    if (classList.contains('expanded text')) {
-        text.classList.remove('expanded text');
-        text.classList.add('hidden text');
+    console.log('toggleText function called with id:', id);
+    if (text.classList.contains('expanded-text')) {
+        text.classList.remove('expanded-text');
+        text.classList.add('hidden-text');
     } else {
-        text.classList.remove('hidden text');
-        text.classList.remove('expanded text');
+        text.classList.remove('hidden-text');
+        text.classList.add('expanded-text');
     }
 }
